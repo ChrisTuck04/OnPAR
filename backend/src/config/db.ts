dac26d4c2cb0f43f0125106c2b4defbb85d57c09
ChrisTuck04 <christopher@tuckman.us>
2025-06-22 16:34:54 -0400

    CI/CD backend testing

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,4 +4,5 @@ export const connectDB = async (): Promise<void> => {
   if (!process.env.MONGO_URI) throw new Error("MONGO_URI not defined");
   await mongoose.connect(process.env.MONGO_URI);
   console.log("MONGODB SUCCESS");
+  
 };
