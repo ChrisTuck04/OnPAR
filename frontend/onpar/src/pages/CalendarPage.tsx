545ef0286ca6871eb612fdb4d046802ce01c886b
Haresh Palli <realhareshpalli@gmail.com>
2025-06-24 17:18:32 -0400

    fixed calendar page error

--- frontend/onpar/src/pages/CalendarPage.tsx
+++ frontend/onpar/src/pages/CalendarPage.tsx
@@ -7,26 +7,27 @@ const CalendarPage = () => {
   const isLoggedIn = !!localStorage.getItem("token");
 
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="fixed bottom-11 z-10">
+          <WholeCalendar/>
+        </div>
 
-      <div className="fixed bottom-11 z-10">
-        <WholeCalendar/>
-      </div>
+        <div className="fixed bottom-0 w-screen">
+          <GolfBackground/>
+        </div>
 
-      <div className="fixed bottom-0 w-screen">
-        <GolfBackground/>
+        <div className="absolute top-4 right-4">
+        <YellowButton to="/" text="Logout"/>
+        </div>
       </div>
-
-      <div className="absolute top-4 right-4">
-      <YellowButton to="/" text="Logout"/>
-      </div>
-    </div>
-    <div className="min-h-screen flex items-center justify-center">
-      <div>
-        <header className="flex justify-between p-4 bg-gray-100">
-          <h1 className="text-xl">My Calendar</h1>
-          {isLoggedIn && <LogoutButton />}
-        </header>
+      <div className="min-h-screen flex items-center justify-center">
+        <div>
+          <header className="flex justify-between p-4 bg-gray-100">
+            <h1 className="text-xl">My Calendar</h1>
+            {isLoggedIn && <LogoutButton />}
+          </header>
+        </div>
       </div>
     </div>
   )
