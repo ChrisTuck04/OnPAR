d66cf5b8f07f52a3c0762828e92b7ca8d03b3c3e
Andres Castro <andresncastro12@gmail.com>
2025-06-24 16:46:20 -0400

    added the work in progress layout for the calendar on the calendar page

--- frontend/onpar/src/pages/CalendarPage.tsx
+++ frontend/onpar/src/pages/CalendarPage.tsx
@@ -1,6 +1,23 @@
+import GolfBackground from "../components/General/GolfBackground"
+import YellowButton from "../components/General/YellowButton"
+import WholeCalendar from "../components/CalendarComponents/WholeCalendar"
+
 const CalendarPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">CalendarPage</div>
+    <div className="min-h-screen flex items-center justify-center">
+
+      <div className="fixed bottom-11 z-10">
+        <WholeCalendar/>
+      </div>
+
+      <div className="fixed bottom-0 w-screen">
+        <GolfBackground/>
+      </div>
+
+      <div className="absolute top-4 right-4">
+      <YellowButton to="/" text="Logout"/>
+      </div>
+    </div>
   )
 }
 
