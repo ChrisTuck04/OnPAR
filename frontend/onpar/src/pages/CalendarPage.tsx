3af3a14821b030b6a5dfef946d7f157f3bb21ba0
Andres Castro <andresncastro12@gmail.com>
2025-06-27 23:45:29 -0400

    created very rough emotions card, will change the colors and styles tomorrow as well as allow theme changing when you choose an emotion

--- frontend/onpar/src/pages/CalendarPage.tsx
+++ frontend/onpar/src/pages/CalendarPage.tsx
@@ -1,15 +1,45 @@
 import LogoutButton from '../components/Logout.tsx';
 import GolfBackground from "../components/General/GolfBackground"
 import WholeCalendar from "../components/CalendarComponents/WholeCalendar"
+import EmotionCard from "../components/EmotionCardComponents/EmotionCard.tsx"
+import { useState } from 'react';
 
 const CalendarPage = () => {
-  const isLoggedIn = !!localStorage.getItem("token");
+  const isLoggedIn = !!localStorage.getItem("token")
+
+  const [emotionCard, setEmotionCard] = useState(true)
+  const [calendar, setCalendar] = useState(false)
+
+  const CloseCard = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setEmotionCard(false)
+    setCalendar(true)
+    e.stopPropagation()
+  }
+
+  const OpenCard = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setEmotionCard(true)
+    setCalendar(false)
+    e.stopPropagation()
+  }
+
+  /*
+  const CloseCalendar = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setCalendar(false)
+    e.stopPropagation()
+  }
+  */
+
+  /*
+  const OpenCalendar = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setCalendar(true)
+    e.stopPropagation()
+  }
+  */
 
   return (
-    <div>
       <div className="min-h-screen flex items-center justify-center">
         <div className="fixed bottom-11 z-10">
-          <WholeCalendar/>
+          {calendar && <WholeCalendar/>}
         </div>
 
         <div className="fixed bottom-0 w-screen">
@@ -19,8 +49,17 @@ const CalendarPage = () => {
         <div className="absolute top-4 right-4">
           {isLoggedIn && <LogoutButton />}
         </div>
+
+        <div className="fixed snap-center z-20">
+          {emotionCard && <EmotionCard CardVisibility={CloseCard}/>}
+        </div>
+
+        <div className="absolute top-4 right-4 font-fredoka" style={{WebkitTextStroke: "1px #FFAA00"}}>
+          <button className="w-[193px] h-[52px] hover:bg-onparOrange border-[3px] border-onparOrange bg-onparLightYellow rounded-2xl p-5 text-l" onClick={OpenCard}>
+            <span className="relative bottom-[6px]">Emotions</span>
+          </button>
+        </div>
       </div>
-    </div>
   )
 }
 
